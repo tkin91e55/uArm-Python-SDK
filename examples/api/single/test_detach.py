78b5835a6c548974a11b6a482c35f1a468b912ac
tkingless <t.kingless@yahoo.com>
2020-02-14 17:36:01 +0800

    tmp

--- examples/api/single/test_detach.py
+++ examples/api/single/test_detach.py
@@ -11,7 +11,6 @@ import sys
 import time
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../..'))
 from uarm.wrapper import SwiftAPI
-#refer to http://download.ufactory.cc/docs/en/uArm-Swift-Pro-Develper-Guide-171013.pdf, page 4, left/rigt from ego of the arm
 from uarm.swift.protocol import SERVO_BOTTOM, SERVO_LEFT, SERVO_RIGHT, SERVO_HAND
 
 """
@@ -30,20 +29,6 @@ print(swift.set_servo_detach(servo_id=SERVO_RIGHT))
 time.sleep(2)
 print(swift.set_servo_detach(servo_id=SERVO_HAND))
 
-time.sleep(2)
-print(swift.set_servo_attach(servo_id=SERVO_BOTTOM))
-time.sleep(2)
-print(swift.set_servo_attach(servo_id=SERVO_LEFT))
-time.sleep(2)
-print(swift.set_servo_attach(servo_id=SERVO_RIGHT))
-time.sleep(2)
-print(swift.set_servo_attach(servo_id=SERVO_HAND))
-
-#deatch all four
-print(swift.set_servo_detach())
-time.sleep(5)
-#attach all four
-print(swift.set_servo_attach())
 time.sleep(2)
 
 swift.disconnect()
