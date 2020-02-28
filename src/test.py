02a82dc3271c694c96503571f83f8df6445504ce
tkin91e55 <t.kingless@yahoo.com>
2020-02-28 19:46:52 +0800

    update polling#

--- src/test.py
+++ src/test.py
@@ -1,19 +1,31 @@
 import sys
 import zmq
-import json
+import zmq.utils.jsonapi as zmqJson
 
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-socket.connect("tcp://localhost:5556")
+socket.connect("tcp://192.168.8.103:5556")
 socket.setsockopt_string(zmq.SUBSCRIBE, "0")
 
+poller = zmq.Poller()
+poller.register(socket, zmq.POLLIN)
+
 shouldRun=True
+
+import json
 def PollSocket():
     while shouldRun:
-        print(shouldRun)
-        jsObj = socket.recv_string()
-        print(jsObj)
 
+        try:
+            polledSockets = dict(poller.poll(timeout=500))
+            if socket in polledSockets and polledSockets[socket] == zmq.POLLIN:
+                jsTxt = socket.recv_string(zmq.DONTWAIT)
+                #print(jsTxt)
+                jsObj=zmqJson.loads(jsTxt.lstrip("0 "))
+                print(jsObj["handType"])
+        except:
+            pass
+         
 import threading
 
 def main():
@@ -26,13 +38,12 @@ def main():
         global shouldRun
         sys.stdin.readline()
         shouldRun = False
-        print("shouldRun False...")
     except KeyboardInterrupt:
         pass
     finally:
         runner.join()
         print("Client disconnecting...")
-        socket.disconnect("tcp://localhost:5556")
+        socket.disconnect("tcp://192.168.8.103:5556")
         context.destroy()
     return
 
